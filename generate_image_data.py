37a29fa14416e245f0f5a424f4befd48387f7157
Alessio Rosiello <alex.robassomero@gmail.com>
2022-12-22 00:02:08 +0100

    Rename function in python script to be more accurate.

--- generate_image_data.py
+++ generate_image_data.py
@@ -2,7 +2,10 @@ from PIL import Image
 import os
 import glob
 
-def make_rgb_565(r, g, b):
+# the LCD should be rgb565 but using rgb555 for the conversion makes it yield better color accuracy.
+# so either the emulator is wrong or the LCD specification is wrong.
+# once I can try it on the actual board I'll see which one is right.
+def make_rgb_555(r, g, b):
     r = ((r & 0xFF) >> 3)
     g = ((g & 0xFF) >> 3)
     b = ((b & 0xFF) >> 3)
@@ -25,7 +28,7 @@ with open('extraPoint1/images/imagedata.h', 'w') as h:
                     matrix_str += '\t{'
                     for j in range(w):
                         r,g,b = im.getpixel((j, i))
-                        matrix_str += f'{make_rgb_565(r, g, b)}{", " if j < w - 1 else " "}'
+                        matrix_str += f'{make_rgb_555(r, g, b)}{", " if j < w - 1 else " "}'
                     matrix_str += '},\n'
                 matrix_str += '};\n'
                 f.write(matrix_str)
