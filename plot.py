ffaa7992d8ad2b4025046463a9f6cd0e19614936
Alessio Rosiello <alex.robassomero@gmail.com>
2023-01-22 12:54:59 +0100

    Finalize project.

--- plot.py
+++ plot.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-y = np.arange(0, 32, 1)
-values = [512,612,708,796,873,937,984,1013,1023,1013,984,937,873,796,708,612,512,412,316,228,151,87,40,11,1,11,40,87,151,228,316,412]
+values = [512, 583, 653, 719, 783, 840, 891, 935, 971, 998, 1015, 1023, 1020, 1008, 985, 954, 914, 866, 811, 751, 686, 618, 547, 475, 404, 337, 271, 211, 156, 108, 68, 37, 14, 2, 0, 7, 24, 51, 87, 131, 182, 241, 303, 370, 440]
+y = np.arange(0, len(values), 1)
 values_sub = [v - values[0] for v in values]
 ex_vol = 0x47
 previous_vol = [v*ex_vol/100 for v in values]
@@ -11,5 +11,7 @@ adj_values = []
 for v in values_sub:
     adj_values.append(int(v*ex_vol/100 + values[0]))
 print(adj_values)
-plt.plot(y, values,y, previous_vol, y, adj_values)
+plt.plot(y, values, label='100% volume')
+plt.plot(y, adj_values, label='71% volume')
+plt.legend()
 plt.show()
